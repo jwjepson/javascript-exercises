2384764366d0a1f774de7329b2e2e75bd45b8525
Jake Jepson <jwjepson1@gmail.com>
2022-08-13 19:30:36 -0400

    give error if args negative or not number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
 const sumAll = function(num1, num2) {
     sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
     if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
             sum = i + sum;
