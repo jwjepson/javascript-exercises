34db136d05b9bca70b3d0ad446ce757153911596
Jake Jepson <jwjepson1@gmail.com>
2022-08-13 16:35:00 -0400

    reverse string and return output

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split("");
+    string = string.reverse();
+    output = string.join("");
+    return output;
 };
 
 // Do not edit below this line
