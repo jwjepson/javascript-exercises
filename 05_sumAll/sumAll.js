de32b60622ea14ae124032fdff10aba935ef5a6a
Jake Jepson <jwjepson1@gmail.com>
2022-08-13 19:23:07 -0400

    calculate the sum between arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    sum = 0;
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum = i + sum;
+        }
+    }
+    else {
+        for (let i = num1; i >= num2; i--) {
+            sum = i + sum;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+123,1
