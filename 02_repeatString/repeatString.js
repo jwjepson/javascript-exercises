4ec0917e0aa6febdd376ffde9a4ab7dbbc381c30
Jake Jepson <jwjepson1@gmail.com>
2022-08-13 14:46:44 -0400

    write repeatString funct and return output

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    for (i = 0; i < num; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
