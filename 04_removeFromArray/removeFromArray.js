cab9e7e03f72d948ad79d6ad97d83234dafa6211
Jake Jepson <jwjepson1@gmail.com>
2022-08-13 17:34:40 -0400

    remove elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        array.splice(array.indexOf(arguments[i]), 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
