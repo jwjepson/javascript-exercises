0eea13d835a0893d9ff8263b475e9c4e31acb534
Jake Jepson <jwjepson1@gmail.com>
2022-08-22 15:59:57 -0400

    write functions to pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sum = array.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return sum };
+
+const multiply = function(array) {
+  const multi = array.reduce((total, num) => {
+    return total * num;
+  }, 1);
+  return multi };
+
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
