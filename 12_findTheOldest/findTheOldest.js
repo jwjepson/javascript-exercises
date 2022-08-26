33c1cbe1a2249c77f2a822f9ee67a379d89c512d
Jake Jepson <jwjepson1@gmail.com>
2022-08-26 18:43:47 -0400

    find the oldest person in array object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = calculateAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentAge = calculateAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 
+    function calculateAge(death, birth) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    };
 };
 
 // Do not edit below this line
