4118c021d97b4c3d0b43b4a104031d770143afb3
Jake Jepson <jwjepson1@gmail.com>
2022-08-26 00:25:46 -0400

    write palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Remove spaces from string and make all lowercase
+    string = string.split(" ").join("").toLowerCase();
 
-};
+    // Remove punctuation from string
+    newString = "";
+    for (letter in string) {
+        if (!(string[letter].includes(".") || string[letter].includes("!") || string[letter].includes(","))) {
+            newString += string[letter];
+        }
+    }
+
+    // Check if string is a palindrome
+    if (newString.split("").reverse().join("") == newString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
